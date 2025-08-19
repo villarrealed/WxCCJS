827a37bfd9986a9b0ad0210d0730a52ba5bcf24a
villarrealed <ed.villarreal@gmail.com>
2025-08-19 13:12:22 -0700

    Create login-alert.js

---
+++ login-alert.js
@@ -0,0 +1,10 @@
+class LoginAlertWidget extends HTMLElement {
+  connectedCallback() {
+    const message = this.getAttribute('message') || 'Welcome!';
+    alert(message);
+    // Optionally redirect after dismissing alert:
+    // window.location.hash = '#/tasks'; // or another page id
+    this.remove();
+  }
+}
+customElements.define('login-alert-widget', LoginAlertWidget);
